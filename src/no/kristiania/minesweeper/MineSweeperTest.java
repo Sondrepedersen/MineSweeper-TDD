310cda6c26fd40ab2756fdb87e8335e4a8c45ce9
Sondrepedersen <Sondreravndal@hotmail.com>
2021-08-23 16:51:36 +0200

    Initial commit

---
+++ src/no/kristiania/minesweeper/MineSweeperTest.java
@@ -0,0 +1,17 @@
+package no.kristiania.minesweeper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MineSweeperTest {
+    @Test
+    void showEmptyField (){
+        assertArrayEquals (new String[] {"000"} , displayField (new String [] {"..."}));
+
+    }
+
+    private String[] displayField(String[] input) {
+        return new String [0];
+    }
+}
